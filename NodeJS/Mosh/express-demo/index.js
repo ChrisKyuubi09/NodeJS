7ea4510cbe35b8adec0c83f8564ed7d018bfaf16
Christos Ioannou <c.ioannou@fameline.com.cy>
2022-01-05 10:43:06 +0200

    Commit Backend

--- NodeJS/Mosh/express-demo/index.js
+++ NodeJS/Mosh/express-demo/index.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const express = require('express');
+const req = require('express/lib/request');
 const app = express();
 
 app.use(express.json());
@@ -8,7 +9,9 @@ const courses = [
     {id:1,name:'Node.JS'},
     {id:2,name:'Maths'},
     {id:3,name:'P.E'}
-]
+];
+
+/////////////////////////////////////////////////////////////////////GET
 app.get('/',(req,res) => {
     res.send('Hello World!!!');
 });
@@ -26,7 +29,7 @@ app.get('/api/courses/:id',(req,res) => {
    res.send(course);
 });
 
-//POST
+/////////////////////////////////////////////////////////////////////POST
 app.post('/api/courses',(req,res) => {
     
     //joi schema
@@ -56,6 +59,55 @@ app.post('/api/courses',(req,res) => {
    res.send(course);
 });
 
+/////////////////////////////////////////////////////////////////////PUT
+
+app.put('/api/courses/:id',(req,res) => {
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if(!course){
+         res.status(404).send('Not found');
+         return;
+    }
+
+    //Normal way    
+    //const result = validateCourse(req.boby);
+
+    //Object Distructuring => Distructure Joi that returns 2 things Message, Error and we need only the Error
+    const { error } = validateCourse(req.body);
+     
+      if (error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+      }
+
+      course.name = req.body.name;
+      res.send(course);
+});
+
+///////////////////////////////////////////////////////////////////DELETE
+app.delete('/api/courses/:id',(req,res) => {
+    const course = courses.find( c=> c.id === parseInt(req.params.id) );
+    if(!course) {
+        res.status(404).send('The course was not found in array');
+        return;
+    }
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+});
+
 //PORT environment variable
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to ${port}...`));
+
+//Validation function
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+      });
+     
+      const result = schema.validate(course);
+      return result;
+}
