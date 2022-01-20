b3aa599f2bc5b3f1400ed35dba6ceb5191710908
ChrisKyuubi09 <chrispilottaras@gmail.com>
2022-01-20 21:10:25 +0200

    Database

--- NodeJS/Mosh/mongo-demo/index.js
+++ NodeJS/Mosh/mongo-demo/index.js
@@ -38,9 +38,105 @@ getCourses();
 async function getCourse() {
     const Course = mongoose.model('Course',courseSchema);
   const courses =  await Course.find( {author:'Mosh'})
-                                .limit(3).sort({name:1}).select({name:1,author:1});
+                                .limit(3).sort({name:1})
+                                .select({name:1,author:1});
   console.log(courses);
 }
 
 getCourse();
 
+
+async function getCoursesOverTen() {
+  const Course = mongoose.model('Course',courseSchema);
+  const courses = await Course.find({price:{$gt:10, $lte:20}}).select({name:1});
+
+  console.log('////////');
+  console.log(courses);
+}
+
+getCoursesOverTen();
+
+async function getCoursesMosh() {
+  const Course = mongoose.model('Course',courseSchema);
+  const courses = await Course.find()
+                              .or([{author:'Mosh'},{isPublished:true}])
+                              .limit(2)
+                              .select({name:1,author:1});
+  console.log(courses);
+}
+getCoursesMosh();
+
+async function RegEx() {
+  const Course = mongoose.model('Course',courseSchema);
+  const courses = await Course.find({author: /^Chris/})
+                              .find({author: /Mosh$/})
+                              .find({author: /.*Mosh.*/i})
+                              .limit(3)
+                              .select({name:1,author:1});
+  console.log('/////');
+  console.log(courses);
+}
+RegEx();
+
+async function CountER() {
+  const Course = mongoose.model('Course',courseSchema);
+  const courses = await Course.find({author: /^Chris/})
+                              .find({author: /Mosh$/})
+                              .find({author: /.*Mosh.*/i})
+                              .limit(3)
+                              .count();
+  console.log('/////');
+  console.log(courses);
+}
+CountER();
+
+async function Paginization() {
+
+  const pageNumber = 2;
+  const pageSize = 10;
+
+  const Course = mongoose.model('Course',courseSchema);
+  const courses = await Course.find({author: /^Chris/})
+                              .find({author: /Mosh$/})
+                              .find({author: /.*Mosh.*/i})
+                              .skip((pageNumber - 1) * pageSize)
+                              .limit(pageSize)
+                              .count();
+  console.log('/////');
+  console.log(courses);
+}
+Paginization();
+
+/////////////////////////////////////////////////////////////////////
+//UPDATE
+
+
+async function update(id) {
+  const Course = mongoose.model('Course',courseSchema);
+  const course = await Course.findById(id);
+
+  if(!course) return console.log('failed');
+    course.set({
+      isPublished: true,
+      author: 'Another Author'
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+update('5a68fde3f09ad7646ddec17e');
+
+async function updateFirst(id) {
+  const Course = mongoose.model('Course',courseSchema);
+  const result = await Course.findByIdAndUpdate(id,{
+    $set: {
+      author:'Jason',
+      isPublished: false
+    }
+  }, { new: true });
+
+  console.log(result);
+}
+
+updateFirst('5a68fde3f09ad7646ddec17e');
